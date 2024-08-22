b7a649f8fdaa01061c42ba60e3cf8cb233ba4de4
renzisen <xj888xj@qq.com>
2024-08-22 09:20:07 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -5,7 +5,10 @@ import os
 
 # 目标URL列表
 urls = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html', 
-        'https://ip.164746.xyz'
+        'https://ip.164746.xyz',
+        "https://cf.090227.xyz/",
+        "https://stock.hostmonit.com/CloudFlareYes",
+        "https://345673.xyz/"
         ]
 
 # 正则表达式用于匹配IP地址
